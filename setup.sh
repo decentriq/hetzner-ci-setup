b161ff36edbd6179b1fc09a7b3dc3d4f0fd17646
exfalso <0slemi0@gmail.com>
2024-12-12 11:48:41 +0100

    wip

--- setup.sh
+++ setup.sh
@@ -3,9 +3,7 @@ set -euo pipefail
 
 # https://raw.githubusercontent.com/decentriq/hetzner-ci-setup/main/setup.sh
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-cd "$SCRIPT_DIR"
+DIR=$(pwd)
 
 mkdir -p build
 
